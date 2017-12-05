5596f59a26d69e33fcc149091488628f69a03a71
Taha Merghani <tahamerghani@TMCFs-MacBook-Pro-5.local>
2017-12-05 14:36:17 -0800

    fixed constraints for pair-wise

--- src/global_model/ilp_solver.py
+++ src/global_model/ilp_solver.py
@@ -4,17 +4,11 @@ import sys
 
 import cplex
 from cplex.exceptions import CplexError
-# F =ψsocial + ψfact +
-# Xn
-# i=1
-# Xn
-# j=1
-# ψ
+
 
 alpha_r, alpha_badr, alpha_itself, alpha_faction, alpha_bl, alpha_badbl = (0.7, -0.8, 0.4, 0.5, 0.1, -0.5)
 
-
-#  For computational
+# For computational
 # efficiency and to avoid erroneous propagation,
 # soft constraints associated with reciprocity
 # and balance theory are introduced only on pairs
@@ -41,9 +35,28 @@ for i in range(1, n):
 			F.variables.add(obj=[alpha_faction]     , names=['tie_same'+str(i)+'_'+str(j)+'_'+str(k)],types='B')
 			F.variables.add(obj=[alpha_faction * -1], names=['tie_diff'+str(i)+'_'+str(j)+'_'+str(k)],types='B')
 
+#ψ_i_j = pairwise potentional
+for i in range(1, n+1):
+	for j in range(1, n+1):
+		F.variables.add(obj=[0.4], names=['pos'+str(i)+'_'+str(j)], types='B')
+		#my_lin.append(cplex.SparsePair(ind=['pos'+str(i)+'_'+str(j)], val=[1]))
+		F.variables.add(obj=[0.4], names=['neg'+str(i)+'_'+str(j)], types='B')
+		#my_lin.append(cplex.SparsePair(ind=['neg'+str(i)+'_'+str(j)], val=[1]))
+		F.variables.add(obj=[0.2], names=['neu'+str(i)+'_'+str(j)], types='B')
+		
+		index = ['pos'+str(i)+'_'+str(j), 'neg'+str(i)+'_'+str(j), 'neu'+str(i)+'_'+str(j)]
+		my_lin.append(cplex.SparsePair(ind=index, val=[1,1,1]))
+		my_sense.append('E')
+		my_rhs.append(1)
+
+F.linear_constraints.add(lin_expr=my_lin, rhs=my_rhs, senses=my_sense)
+
+		#names.append(['pair'+str(i)+'_'+str(j)])
+		#print (my_lin, my_rhs)
+
 #ψ_r = reciprocitiy constraint
 for i in range(1, n+1):
-	for j in range(1,n+1):
+	for j in range(1, n+1):
 		F.variables.add(obj=[alpha_r], names=['r_same'+str(i)+'_'+str(j)],types='B')
 		F.variables.add(obj=[alpha_badr], names=['r_diff'+str(i)+'_'+str(j)],types='B')
 
@@ -56,20 +69,8 @@ for i in range(1, n+1):
 			F.variables.add(obj=[alpha_badbl], names=['pos_diff'+str(i)+'_'+str(j)+'_'+str(k)],types='B')
 			F.variables.add(obj=[alpha_badbl], names=['neg_name'+str(i)+'_'+str(j)+'_'+str(k)],types='B')
 
-for i in range(1, n+1):
-	for j in range(1, n+1):
-		F.variables.add(obj=[0.4], names=['pos'+str(i)+'_'+str(j)],types='B')
-		my_lin.append(cplex.SparsePair(ind=['pos'+str(i)+'_'+str(j)], val=[1]))
-		F.variables.add(obj=[0.4], names=['neg'+str(i)+'_'+str(j)],lb=[0], ub=[1],types='B')
-		my_lin.append(cplex.SparsePair(ind=['neg'+str(i)+'_'+str(j)], val=[1]))
-		F.variables.add(obj=[0.2], names=['neu'+str(i)+'_'+str(j)],lb=[0], ub=[1],types='B')
-		my_lin.append(cplex.SparsePair(ind=['neu'+str(i)+'_'+str(j)], val=[1]))
-		my_sense.append('E')
-		my_rhs.append(1)
-		names.append(['pair'+str(i)+'_'+str(j)])
-		#print (my_lin, my_rhs)
 F.write('example.lp')
-#F.linear_constraints.add(names = names, lin_expr= my_lin, rhs=my_rhs, senses=my_sense)
+
 
 
 try:
